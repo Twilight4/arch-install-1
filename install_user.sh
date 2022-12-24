c764b33132f798a242e4934c87db219cd1771f83
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-24 17:54:53 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -84,8 +84,7 @@ install-dotfiles() {
     sudo rm "/home/$(whoami)/.config/.local/share/fonts/README.md"
     sudo fc-cache -f
     sudo rm /home/$(whoami)/.bash*
-    sudo mv /tmp/dotfiles/.local/bin/wrappedh1 "/home/$(whoami)/.local/bin/"
-    sudo chmod 755 $HOME/.local/bin/wrappedh1
+    sudo chmod 755 $HOME/.config/.local/bin/wrappedh1
     sudo chmod 755 $XDG_CONFIG_HOME/hypr/scripts/*
     #sudo chmod 755 $XDG_CONFIG_HOME/polybar/launch.sh              # outdated
     #sudo chmod 755 $HOME/.config/polybar/polybar-scripts/*         # outdated
