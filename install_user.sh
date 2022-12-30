6e4f4ebf3ed955b80b0e21153f1043421263c47f
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-30 16:59:25 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -72,6 +72,7 @@ install-apps() {
     sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
     sudo pacman-key --lsign-key FBA220DFC880C036
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+    sudo pacman -Sy
 }
 
 enable-blackarch() {
