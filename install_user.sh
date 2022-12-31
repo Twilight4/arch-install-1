1c59c81a720afee74ebbb29420b6efd1d8fb794a
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-31 19:10:43 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -5,7 +5,6 @@ run() {
     download-paclist
     download-yaylist
     install-yay
-    #install-wifi-driver    # for my TP-Link wifi adapter - check if aur package works instead of building manually
     install-apps
     enable-blackarch
     create-directories
@@ -38,18 +37,6 @@ install-yay() {
     && makepkg --noconfirm -si \
     && cd - \
     && rm -rf yay-bin
-}
-
-install-wifi-driver() {
-    sudo pacman -S --noconfirm dkms 
-    git clone https://github.com/Mange/rtl8192eu-linux-driver \
-    && cd rtl8192eu-linux-driver \
-    && sudo dkms add . \
-    && sudo dkms install rtl8192eu/1.0 \
-    && echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf \
-    && echo -e "8192eu\n\nloop" | sudo tee /etc/modules \
-    && echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf \
-    && sudo bootctl update \
 }
 
 install-apps() {
