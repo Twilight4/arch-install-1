6e2247fc1eb36c9282d7ce9eba9662db86e72087
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-07 14:35:28 +0100

    Update all-in-one.sh

--- all-in-one.sh
+++ all-in-one.sh
@@ -327,7 +327,7 @@ microcode_detector
 
 # Pacstrap (setting up a base sytem onto the new root)
 info_print "Installing the base system."
-pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo &>/dev/null
+pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers grub grub-btrfs snapper snap-pac efibootmgr sudo reflector networkmanager apparmor zram-generator pipewire-pulse pipewire-alsa firewalld chrony &>/dev/null
 
 # Setting up the hostname
 echo "$hostname" > /mnt/etc/hostname
